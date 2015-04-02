d6b8490615889e7a04fcb226769f536347a7e986
Will <wpgervasio@gmail.com>
2015-04-01 22:38:16 -0400

    added a function to draw images

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function drawImage(context, image, x, y, rotation)
 	context.rotate(rotation);
 
 	context.drawImage(image, -(image.width / 2), -(image.height / 2));
-	
+
 	context.restore();
 };
 
@@ -16,5 +16,3 @@ var aabb1 = new AABB(0, 0, 2, 2);
 var aabb2 = new AABB(3, 3, 6, 6);
 
 window.alert(aabb1.collidesWithAABB(aabb2));
-
-var running = true;
